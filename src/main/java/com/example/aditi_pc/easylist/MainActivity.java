7c94a84356382eb60c99d256a66f4cba280e07be
ApoorvAga <agarwalapoorv.1999@gmail.com>
2017-07-04 00:39:48 +0530

    Add files via upload

---
+++ src/main/java/com/example/aditi_pc/easylist/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.aditi_pc.easylist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void addNewList(View view) {
+    }
+}
